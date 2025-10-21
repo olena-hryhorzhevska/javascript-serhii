cd2bffe8d0b22fb3218e143e383282d1f2539898
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-10-21 12:54:53 -0400

    For in & for of

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,71 +1,101 @@
-// MAP
+// // MAP
 
-const prices = [10, 20, 30];
-const newPrices = prices.map(price => {
-  return price * 1.1
-});
-console.log(newPrices);
-
-
-// FILTER
-const ages = [15, 32, 45, 12, 76];
-const adults = ages.filter(age => age >= 18);
-console.log(adults);
+// const prices = [10, 20, 30];
+// const newPrices = prices.map(price => {
+//   return price * 1.1
+// });
+// console.log(newPrices);
 
 
-
-
-// FIND (находит первого, который соответствует условию и отдает его)
-const users = [
-  {
-    name: 'Anna',
-    age: 18,
-  },
-  {
-    name: 'Dima',
-    age: 25,
-  },
-  {
-    name: 'Ksusha',
-    age: 15,
-  },
-];
-
-const firstAdult = users.find(user => { return user.age >= 18 });
-console.log(firstAdult);
+// // FILTER
+// const ages = [15, 32, 45, 12, 76];
+// const adults = ages.filter(age => age >= 18);
+// console.log(adults);
 
 
 
 
-// forEach
+// // FIND (находит первого, который соответствует условию и отдает его)
+// const users = [
+//   {
+//     name: 'Anna',
+//     age: 18,
+//   },
+//   {
+//     name: 'Dima',
+//     age: 25,
+//   },
+//   {
+//     name: 'Ksusha',
+//     age: 15,
+//   },
+// ];
 
-const fruits = ['apple', 'banana', 'orange'];
-fruits.forEach(fruit => console.log('🍁', fruit))
-
-
-// SOME(), EVERY()
-
-const nums = [2, 4, 6, 9];
-console.log(nums.some(num => num > 5));
-console.log(nums.every(num => num % 2 === 0));
-
-
-// REDUCE()
-
-const numbers1 = [5, 10, 15];
-const total = numbers1.reduce((sum, num) => sum + num,);
-console.log(total);
+// const firstAdult = users.find(user => { return user.age >= 18 });
+// console.log(firstAdult);
 
 
 
-// SORT()
 
-const numbers = [15, 34, 12, 67];
-const sortedNumbers = [...numbers].sort((a, b) => a - b); // asc order
-console.log(sortedNumbers);
+// // forEach
 
-const fruits2 = ['banana', 'Apple', 'kiwi', 'cherry'];
-const sortedFruits = [...fruits2].sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()))
-console.log(sortedFruits);
+// const fruits = ['apple', 'banana', 'orange'];
+// fruits.forEach(fruit => console.log('🍁', fruit))
+
+
+// // SOME(), EVERY()
+
+// const nums = [2, 4, 6, 9];
+// console.log(nums.some(num => num > 5));
+// console.log(nums.every(num => num % 2 === 0));
+
+
+// // REDUCE()
+
+// const numbers1 = [5, 10, 15];
+// const total = numbers1.reduce((sum, num) => sum + num,);
+// console.log(total);
+
+
+
+// // SORT()
+
+// const numbers = [15, 34, 12, 67];
+// const sortedNumbers = [...numbers].sort((a, b) => a - b); // asc order
+// console.log(sortedNumbers);
+
+// const fruits2 = ['banana', 'Apple', 'kiwi', 'cherry'];
+// const sortedFruits = [...fruits2].sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()))
+// console.log(sortedFruits);
+
+
+
+
+// // SPLIT()
+// // string.split(separator);
+
+// const sentence = 'JavaScript - is - fun'
+// const words = sentence.split(' - ');
+// console.log(words);
+
+
+// const email = 'serhii@gmail.com'
+// const parts = email.split('@');
+// console.log(parts);
+
+
+// // JOIN()
+// // array.join(separator);
+
+
+// const words2 = ['JavaScript', 'is', 'fun']
+// const sentence2 = words2.join(' ');
+// console.log(sentence2);
+
+
+// const digits = ['123', '456', '789'];
+// const phone = digits.join('-');
+// console.log(phone);
+
 
 
--- loops.js
+++ loops.js
@@ -1,79 +1,159 @@
-// WHILE
+// // WHILE
 
-let i = 0;
-while (i < 5) {
-  console.log(i);
-  i++;
-}
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
 
-let clients = 18;
-const maxClients = 25;
+// let clients = 18;
+// const maxClients = 25;
 
-while (clients < maxClients) {
-  console.log('Client number:', clients);
-  clients++;
-}
+// while (clients < maxClients) {
+//   console.log('Client number:', clients);
+//   clients++;
+// }
 
-// DO...WHILE
+// // DO...WHILE
 
-// let count = 4;
-// do {
-//   console.log(count);
-//   ++count;
-// } while (count <= 3);
+// // let count = 4;
+// // do {
+// //   console.log(count);
+// //   ++count;
+// // } while (count <= 3);
 
-// let n = 56;
-// let n2 = n++;
-// console.log(n);
-// console.log(n2);
-
-
-// let n1 = 56;
-// let n3 = ++n1;
-// console.log(n1);
-// console.log(n3);
+// // let n = 56;
+// // let n2 = n++;
+// // console.log(n);
+// // console.log(n2);
 
 
-
-// let x = 10;
-// do {
-//   console.log('x=', x);
-//   x++
-// } while (x < 5)
+// // let n1 = 56;
+// // let n3 = ++n1;
+// // console.log(n1);
+// // console.log(n3);
 
 
 
+// // let x = 10;
+// // do {
+// //   console.log('x=', x);
+// //   x++
+// // } while (x < 5)
 
-// FOR
+
+
+
+// // FOR
   
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+// // for (let i = 0; i <= 10; i++) {
+// //   console.log(i);
+// // }
 
 
-// for (let i = 0; i <= 10; i += 2) {
-//   console.log(i);
-// }
+// // for (let i = 0; i <= 10; i += 2) {
+// //   console.log(i);
+// // }
 
 
-// BREAK
+// // BREAK
 
-// for (let i = 0; i < 10; i++) {
-//   if (i === 5) {
+// // for (let i = 0; i < 10; i++) {
+// //   if (i === 5) {
+// //     break;
+// //   }
+// //   console.log(i);
+// // }
+
+// const nums4 = [4, 7, -2, -6]; // 4
+// let index = 0;
+
+// while (index < nums4.length) {
+
+//   if (nums4[index] < 0) {
 //     break;
 //   }
-//   console.log(i);
+//   console.log(nums4[index]);
+//   index++;
 // }
 
-const nums4 = [4, 7, -2, -6]; // 4
-let index = 0;
 
-while (index < nums4.length) {
 
-  if (nums4[index] < 0) {
-    break;
+// FOR...OF
+const orders = [
+  {
+    id: 1,
+    total: 120,
+    status: 'completed'
+  },
+  {
+    id: 2,
+    total: 80,
+    status: 'pending'
+  },
+  {
+    id: 3,
+    total: 200,
+    status: 'completed'
+  },
+  {
+    id: 4,
+    total: 50,
+    status: 'canceled'
   }
-  console.log(nums4[index]);
-  index++;
+]
+
+let completedTotal = 0;
+
+for (const order of orders) {
+  if (order.status === 'completed') {
+    completedTotal += order.total;
+  }
+}
+console.log(`Total amount of completed orders: ${completedTotal}`);
+
+
+
+const user = {
+  name: 'Serhii',
+};
+
+const requiredFields = ['name', 'email', 'age'];
+let missing = [];
+
+for (const key of requiredFields) {
+  if (!(key in user)) {
+    missing.push(key)
+  }
 }
 
+if (missing.length > 0) {
+  console.log(`There is no field: ${missing.join(", ")}`);
+}
+else {
+  console.log('All necessary fields exist');
+}
+
+
+
+
+
+// FOR...IN
+
+// const user = {
+//   name: 'Serhii',
+//   email: 'serhii@gmail.com',
+//   age: 25,
+// }
+
+// let hasEmpty = false;
+
+// for (const key in user) {
+//   if (user[key] === '' || user[key] === null || user[key] === undefined) {
+//     console.log(`Поле ${key} не заполнено!`);
+//     hasEmpty = true;
+//   }
+// }
+// if (!hasEmpty) {
+//   console.log('All the fields filled!');
+// }
