1e097c04a8dc87792eee749e5e82aafafa67f22c
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-10-23 12:43:07 -0400

    loop in a loop

--- loops.js
+++ loops.js
@@ -27,13 +27,10 @@
 // // console.log(n);
 // // console.log(n2);
 
-
 // // let n1 = 56;
 // // let n3 = ++n1;
 // // console.log(n1);
 // // console.log(n3);
-
-
 
 // // let x = 10;
 // // do {
@@ -41,20 +38,15 @@
 // //   x++
 // // } while (x < 5)
 
-
-
-
 // // FOR
-  
+
 // // for (let i = 0; i <= 10; i++) {
 // //   console.log(i);
 // // }
 
-
 // // for (let i = 0; i <= 10; i += 2) {
 // //   console.log(i);
 // // }
-
 
 // // BREAK
 
@@ -77,66 +69,57 @@
 //   index++;
 // }
 
-
-
 // FOR...OF
-const orders = [
-  {
-    id: 1,
-    total: 120,
-    status: 'completed'
-  },
-  {
-    id: 2,
-    total: 80,
-    status: 'pending'
-  },
-  {
-    id: 3,
-    total: 200,
-    status: 'completed'
-  },
-  {
-    id: 4,
-    total: 50,
-    status: 'canceled'
-  }
-]
+// const orders = [
+//   {
+//     id: 1,
+//     total: 120,
+//     status: 'completed',
+//   },
+//   {
+//     id: 2,
+//     total: 80,
+//     status: 'pending',
+//   },
+//   {
+//     id: 3,
+//     total: 200,
+//     status: 'completed',
+//   },
+//   {
+//     id: 4,
+//     total: 50,
+//     status: 'canceled',
+//   },
+// ];
 
-let completedTotal = 0;
+// let completedTotal = 0;
 
-for (const order of orders) {
-  if (order.status === 'completed') {
-    completedTotal += order.total;
-  }
-}
-console.log(`Total amount of completed orders: ${completedTotal}`);
+// for (const order of orders) {
+//   if (order.status === 'completed') {
+//     completedTotal += order.total;
+//   }
+// }
+// console.log(`Total amount of completed orders: ${completedTotal}`);
 
+// const user = {
+//   name: 'Serhii',
+// };
 
+// const requiredFields = ['name', 'email', 'age'];
+// let missing = [];
 
-const user = {
-  name: 'Serhii',
-};
+// for (const key of requiredFields) {
+//   if (!(key in user)) {
+//     missing.push(key);
+//   }
+// }
 
-const requiredFields = ['name', 'email', 'age'];
-let missing = [];
-
-for (const key of requiredFields) {
-  if (!(key in user)) {
-    missing.push(key)
-  }
-}
-
-if (missing.length > 0) {
-  console.log(`There is no field: ${missing.join(", ")}`);
-}
-else {
-  console.log('All necessary fields exist');
-}
-
-
-
-
+// if (missing.length > 0) {
+//   console.log(`There is no field: ${missing.join(', ')}`);
+// } else {
+//   console.log('All necessary fields exist');
+// }
 
 // FOR...IN
 
@@ -157,3 +140,20 @@ else {
 // if (!hasEmpty) {
 //   console.log('All the fields filled!');
 // }
+
+
+
+
+// 1 * 1 = 1
+// 1 * 2 = 2
+// 1 * 3 = 3
+// 1 * 4 = 4
+// 1 * 5 = 5
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+// 2 * 5 = 10
+
+
+//  
