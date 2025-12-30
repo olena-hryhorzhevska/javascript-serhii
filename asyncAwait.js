1ae969c3462910cf22cd44d7c866ecfcf56b028b
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-12-30 12:24:25 -0500

    promises and async await

---
+++ asyncAwait.js
@@ -0,0 +1,52 @@
+// Function declaration
+// async function fetchData() {
+
+// }
+
+// Function expression
+// const fetchData = async function () {
+
+// }
+
+// Arrow function
+// const fetchData = async () => {
+
+// }
+
+// data();
+// function data() {
+//   console.log('Data fetched successfully');
+// }
+
+// data();
+// const data = function () {
+//   console.log('Data fetched successfully');
+// }
+
+// const data;
+// console.log(data);
+
+
+
+// const foo = async () => {
+//   return 5; // Возвращает промис, который резолвится в 5 Promise.resolve(5)
+// }
+
+// foo().then(value => {
+//   console.log(value); // 5
+// });
+
+
+// const foo = async () => {
+//   console.log('Начало функции foo');
+//   const promiseValue = await new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve('Результат промиса из foo');
+//     }, 2000);
+//   })
+//   console.log('После await в функции foo', promiseValue);
+// }
+// foo()
+
+
+
--- promise.js
+++ promise.js
@@ -1,12 +1,11 @@
+
+
 // const data = fetchData();
 // console.log(data);
 
-
-
 // Promise live cycle
 // Pending -> Fulfilled or Rejected
-// settled 
-
+// settled
 
 // const promise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
@@ -20,8 +19,6 @@
 //   console.log('Через 3 секунды после создания промиса', promise);
 // }, 3000);
 
-
-
 // const errorPromise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     reject('Ошибка при получении данных');
@@ -34,16 +31,12 @@
 //   console.log('Через 3 секунды после создания промиса', errorPromise); // Rejected
 // }, 3000);
 
-
-
 // const promise = new Promise((resolve, reject) => {
-  
+
 // })
 
 // resolve(value) - успешное выполнение
 // reject(reason) - ошибка выполнения
-
-
 
 // const isSuccess = true;
 // const promise = new Promise((resolve, reject) => {
@@ -58,7 +51,6 @@
 
 // console.log('Сразу после создания промиса', promise); // Pending
 
-
 // then - catch - finally
 // then - для обработки успешного выполнения
 
@@ -68,7 +60,6 @@
 
 // console.log(promise); // Fullfiled Promise
 
-
 // promise.then(value => {
 //   console.log('Promise fulfilled with value:', value);
 //   return value * 2;
@@ -76,7 +67,6 @@
 //   .then(value => {
 //   console.log('Второй then:', value);
 // })
-
 
 // catch - для обработки ошибки
 
@@ -97,7 +87,6 @@
 //   .catch(error => {
 //   console.log('Promise rejected with reason:', error);
 // })
-
 
 // finally - для выполнения кода в любом случае
 
@@ -122,7 +111,6 @@
 //   console.log('Promise settled (either fulfilled or rejected)');
 // })
 
-
 // Цепочки промисов
 // Promise.resolve("start")
 //   .then((value) => {
@@ -140,8 +128,6 @@
 //     console.log('Обработка ошибки:', error.message);
 //   })
 
-
-
 // ❗️Промисификация функций с колбэками
 // function delayCallback(ms, callback) {
 //   setTimeout(() => {
@@ -152,7 +138,6 @@
 // delayCallback(2000, (message) => {
 //   console.log(message);
 // });
-
 
 // function delayPromise(ms) {
 //   return new Promise((resolve) => {
@@ -165,7 +150,6 @@
 // delayPromise(2000)
 //   .then(message => console.log(message));
 
-
 // Promise.resolve('Успешное значение')
 //   .then(value => {
 //   console.log('Первый then:', value);
@@ -174,7 +158,6 @@
 //   console.log('Обработка ошибки:', error);
 //   })
 
-
 //   Promise.reject('Ошибка выполнения')
 //     .then(value => {
 //       console.log('Первый then:', value);
@@ -182,7 +165,6 @@
 //     .catch(error => {
 //       console.log('Обработка ошибки:', error);
 //     });
-
 
 // Промисификация синхронных функций
 
@@ -194,12 +176,11 @@
 //     onSuccess(`Привет, ${name}!`);
 //     }
 // }
-  
+
 // makeGreeting('',
 //   greeting => console.log(greeting),
 //   error => console.log('Ошибка:', error)
 // )
-
 
 // const makeGreetingPromise = (name) => {
 //   return new Promise((resolve, reject) => {
@@ -214,3 +195,91 @@
 // makeGreetingPromise('')
 //   .then(greeting => console.log(greeting))
 //   .catch(error => console.log('Ошибка:', error));
+
+
+// Работа с несколькими промисами
+// Promise.all, Promise.allSettled, Promise.race
+
+
+
+// ❗️Promise.all
+// Promise.all([promise1, promise2, promise3])
+// на выходе массив со значениями всех выполненных промисов и если хоть один упал, то статус всего промиса будет rejected
+
+// const p1 = Promise.resolve('Первый промис выполнен');
+// const p2 = Promise.resolve('Второй промис выполнен');
+// const p3 = Promise.resolve('Третий промис выполнен');
+
+// // p1.then(value => console.log(value));
+
+// Promise.all([p1, p2, p3])
+//   .then(value => {
+//   console.log('Все промисы выполнены:', value);
+//   })
+//   .catch(error => {
+//   console.log('Один из промисов отклонен:', error);
+// })
+
+
+// const p1 = Promise.resolve('Первый промис выполнен');
+// const p2 = Promise.reject('Второй промис отклонен');
+// const p3 = Promise.resolve('Третий промис выполнен');
+
+
+// Promise.all([p1, p2, p3])
+//   .then(value => {
+//   console.log('Все промисы выполнены:', value);
+//   })
+//   .catch(error => {
+//   console.log('Один из промисов отклонен:', error);
+//   })
+
+
+
+// ❗️Promise.allSettled
+// Promise.allSettled([promise1, promise2, promise3])
+// на выходе массив со статусами и значениями/причинами отклонения и статус промиса fulfilled|
+
+
+// const p1 = Promise.resolve('Первый промис выполнен');
+// const p2 = Promise.reject('Второй промис отклонен');
+// const p3 = Promise.resolve('Третий промис выполнен');
+
+// Promise.allSettled([p1, p2, p3])
+//   .then(results => {
+//   console.log('Результаты всех промисов:', results);
+// })
+
+// Promise.allSettled([p1, p2, p3])
+//   .then(results => {
+//     const success = results.filter(item => item.status === 'fulfilled')
+//       .map(item => item.value);
+//     console.log('Успешные результаты:', success);
+    
+// })
+
+
+// ❗️Promise.race()
+// Promise.race([promise1, promise2, promise3])
+// возвращает результат первого выполненного или отклоненного промиса из переданного массива промисов
+
+// const p1 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('1');
+//   }, 2000);
+// })
+
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject('2');
+//   }, 1000);
+// })
+
+
+// Promise.race([p1, p2])
+//   .then(value => {
+//   console.log('Первый выполненный промис:', value);
+//   })
+//   .catch(error => {
+//   console.log('Один из промисов отклонен:', error);
+// })
